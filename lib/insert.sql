027027d1b10d7e3d9421572406096d1f8b1f7754
Winston  Chung <18.chung.winston@ctkprep.org>
2019-10-29 21:09:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,73 @@
+INSERT INTO projects VALUES (1, "space force", "space", 12000000000, 12/24, 1/28 );
+
+INSERT INTO projects VALUES (2, "make america great again", "economics", 50000000000000, 12/24, 1/42 );
+
+INSERT INTO projects VALUES (3, "affordable housing", "economics", 2000000000, 11/22, 8/28 );
+
+INSERT INTO projects VALUES (4, "buy me a house", "personal", 420000, 12/19, 2/20 );
+
+INSERT INTO projects VALUES (5, "new open source processor", "electronics", 600000, 12/20, 5/36 );
+
+INSERT INTO projects VALUES (6, "need a new laptop", "personal", 6000, 1/20, 4/20 );
+
+INSERT INTO projects VALUES (7, "elctric car shop", "automotive", 700000, 12/21, 4/28 );
+
+INSERT INTO projects VALUES (8, "variable ram technology", "electronics", 40000, 2/24, 7/28 );
+
+INSERT INTO projects VALUES (9, "open source satellite", "space", 5000000000, 7/22, 8/30 );
+
+INSERT INTO projects VALUES (10, "county tax reduction", "economics", 5000000, 5/20, 6/25 );
+
+
+INSERT INTO users VALUES (1, "brittany spears", 50);
+INSERT INTO users VALUES (2, "winston chung", 82);
+INSERT INTO users VALUES (3, "king neptune ", 500);
+INSERT INTO users VALUES (4, "insta bot", 5);
+INSERT INTO users VALUES (5, " frank philip", 23);
+INSERT INTO users VALUES (6, "josh posh", 28);
+INSERT INTO users VALUES (7, "island rhode", 19);
+INSERT INTO users VALUES (8, "justin granite", 50);
+INSERT INTO users VALUES (9, "forgone soule", 49);
+INSERT INTO users VALUES (10, "usher williamson", 36);
+INSERT INTO users VALUES (11, "scarlett beam", 26);
+INSERT INTO users VALUES (12, "brianna garcia", 32);
+INSERT INTO users VALUES (13, "mentol floss", 70);
+INSERT INTO users VALUES (14, "oprah oprah", 34);
+INSERT INTO users VALUES (15, "marchian one", 1970);
+INSERT INTO users VALUES (16, "ohmi nomey", 24);
+INSERT INTO users VALUES (17, "john habrey", 72);
+INSERT INTO users VALUES (18, "costo riley", 48);
+INSERT INTO users VALUES (19, "plain jane", 27);
+INSERT INTO users VALUES (20, "rich guy fly guy", 41);
+
+
+INSERT INTO pledges VALUES (1, 40000, 6, 10);
+INSERT INTO pledges VALUES (2, 400000, 6, 10);
+INSERT INTO pledges VALUES (3, 2000, 1, 10);
+INSERT INTO pledges VALUES (4, 300, 3, 10);
+INSERT INTO pledges VALUES (5, 2000, 5, 10);
+INSERT INTO pledges VALUES (6, 4562, 7, 10);
+INSERT INTO pledges VALUES (7, 20000000, 8, 10);
+INSERT INTO pledges VALUES (8, 4321, 7, 10);
+INSERT INTO pledges VALUES (9, 674, 5, 10);
+INSERT INTO pledges VALUES (10, 7453, 4, 10);
+INSERT INTO pledges VALUES (11, 80000, 7, 10);
+INSERT INTO pledges VALUES (12, 24, 1, 10);
+INSERT INTO pledges VALUES (13, 400000, 11, 10);
+INSERT INTO pledges VALUES (14, 2000000, 14, 10);
+INSERT INTO pledges VALUES (15, 59322, 10, 10);
+INSERT INTO pledges VALUES (16, 600, 15, 10);
+INSERT INTO pledges VALUES (17, 9432, 19, 10);
+INSERT INTO pledges VALUES (18, 755, 19, 10);
+INSERT INTO pledges VALUES (19, 7457, 20, 10);
+INSERT INTO pledges VALUES (20, 45234, 18, 10);
+INSERT INTO pledges VALUES (21, 45, 9, 10);
+INSERT INTO pledges VALUES (22, 7243, 6, 10);
+INSERT INTO pledges VALUES (23, 465, 13, 10);
+INSERT INTO pledges VALUES (24, 4457, 17, 10);
+INSERT INTO pledges VALUES (25, 4235, 16, 10);
+INSERT INTO pledges VALUES (26, 23234, 19, 10);
+INSERT INTO pledges VALUES (27, 54324, 2, 10);
+INSERT INTO pledges VALUES (28, 545, 12, 10);
+INSERT INTO pledges VALUES (29, 4745, 11, 10);
+INSERT INTO pledges VALUES (30, 785, 17, 10);
