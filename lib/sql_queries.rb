66594ea421396fc637b990a1bfe0e0bda615b187
Winston  Chung <18.chung.winston@ctkprep.org>
2019-10-29 20:43:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,9 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT name, sum(pledges.amount)
-  FROM users"
+  FROM users
+  INNER JOIN pledges ON users.id = pledges.user_id
+  GROUP BY name "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
