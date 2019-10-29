d3895382a66e0f46358f281a031b6e006684aece
Winston  Chung <18.chung.winston@ctkprep.org>
2019-10-29 21:01:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,10 +38,9 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT category, sum(pledges.amount)
+  "SELECT category, sum(pledges.amount) WHERE category = 'music'
   FROM projects
-  INNER JOIN pledges ON projects.id = pledges.project_id
-  GROUP BY category HAVING category = 'music';"
+  INNER JOIN pledges ON projects.id = pledges.project_id;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
